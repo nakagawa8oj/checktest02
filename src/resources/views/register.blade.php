98ca5643d9aca074a42a4055d6972e64409d65a8
nakagawa8oj <tsk9n23ds2302@gmail.com>
2025-05-06 16:01:58 +0900

    リモートリポジトリの変更

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -23,56 +23,62 @@
       <form class="form">
         <div class="form__group">
           <div class="form__group-title">
-            <span class="form__label--item">お名前</span>
+            <span class="form__label--item">商品名</span>
             <span class="form__label--required">必須</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="text" name="name" placeholder="テスト太郎" />
+              <input type="text" name="name" placeholder="商品名を入力" />
             </div>
             <div class="form__error">
               <!--バリデーション機能を実装したら記述します。-->
             </div>
           </div>
         </div>
+
         <div class="form__group">
           <div class="form__group-title">
-            <span class="form__label--item">メールアドレス</span>
+            <span class="form__label--item">値段</span>
             <span class="form__label--required">必須</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="email" name="email" placeholder="test@example.com" />
+              <input type="tel" name="tel" placeholder="値段を入力" />
             </div>
             <div class="form__error">
               <!--バリデーション機能を実装したら記述します。-->
             </div>
           </div>
         </div>
+        
         <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">電話番号</span>
-            <span class="form__label--required">必須</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--text">
-              <input type="tel" name="tel" placeholder="09012345678" />
-            </div>
-            <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
-            </div>
+          <div class="form__group-title">      
+            <span class="form__label--item">季節</span>
+            <span class="form__label--required">必須  複数選択可</span>
+            <br>
+              <input type="radio" name="choices" value="春" />春
+              <input type="radio" name="choices" value="夏" />夏
+              <input type="radio" name="choices" value="秋" />秋
+              <input type="radio" name="choices" value="冬" />冬
+            <br>
           </div>
         </div>
+
+
         <div class="form__group">
           <div class="form__group-title">
-            <span class="form__label--item">お問い合わせ内容</span>
+            <span class="form__label--item">商品説明</span>
           </div>
           <div class="form__group-content">
             <div class="form__input--textarea">
-              <textarea name="content" placeholder="資料をいただきたいです"></textarea>
+              <textarea name="content" placeholder="商品の説明を入力"></textarea>
             </div>
           </div>
         </div>
+        <form action="/products" method="get">
+      @csrf
+      <button class="header-nav__button">戻る</button>
+        </form>
         <div class="form__button">
           <button class="form__button-submit" type="submit">登録</button>
         </div>
